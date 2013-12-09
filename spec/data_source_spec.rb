3a41ef7410a8a7954ef6fb08203111c54ed48134
SpencerArtisan <spencerartisan@gmail.com>
2013-12-09 15:37:38 +0000

    Tidy data source tests

--- spec/data_source_spec.rb
+++ spec/data_source_spec.rb
@@ -3,12 +3,23 @@ require 'data_source'
 class FindAnyFilm; end
 
 describe DataSource do
-  it 'should get the films from findanyfilm.com' do
-    find_any_film_sample = File.read 'find_any_film_sample.json'
-    allow(FindAnyFilm).to receive(:get_films).with(1, 1).and_return find_any_film_sample
-    films = DataSource.get_films(1, 1)
-    expect(films.size).to eq 6
-    expect(films[0].title).to eq 'Gone With The Wind (1939) (PG)'
-    expect(films[0].year).to eq 1939
+  context 'Reading the films' do
+    before do
+      find_any_film_sample = File.read 'find_any_film_sample.json'
+      allow(FindAnyFilm).to receive(:get_films).with(1, 1).and_return find_any_film_sample
+      @films = DataSource.get_films(1, 1)
+    end
+
+    it 'should get all the films' do
+      expect(@films.size).to eq 6
+    end
+
+    it 'should get the film title' do
+      expect(@films[0].title).to eq 'Gone With The Wind (1939) (PG)'
+    end
+
+    it 'should get the film year' do
+      expect(@films[0].year).to eq 1939
+    end
   end
 end
