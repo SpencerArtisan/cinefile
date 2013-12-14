357aca501690fbe85818a92cc4fd8c067342d647
SpencerArtisan <spencerartisan@gmail.com>
2013-12-14 20:17:30 +0100

    Rotten tomatoes provides link

---
+++ lib/rotten_tomatoes.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'rest-client'
+
+class RottenTomatoes
+  KEY = "khyrfh8p43auq75j5eh66gae"
+
+  def get_details film
+    url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{KEY}&q=#{CGI::escape(film.title)}&page_limit=1"
+    result = RestClient.get url
+
+    json = JSON.parse result
+    link = json["movies"][0]["links"]["alternate"]
+    return OpenStruct.new link: link
+  end
+end
---
+++ spec/rotten_tomatoes_spec.rb
@@ -0,0 +1,12 @@
+require 'rotten_tomatoes'
+
+describe RottenTomatoes do
+  let (:film) { double }
+  let (:rotten_tomatoes) { RottenTomatoes.new }
+
+  it 'should find the film' do
+    allow(film).to receive(:title).and_return 'Gone With The Wind (1939)'
+    details = rotten_tomatoes.get_details film
+    expect(details.link).to eq 'http://www.rottentomatoes.com/m/gone_with_the_wind/'
+  end
+end
