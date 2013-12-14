4da9139ec09c00534912080fdaf49ab811edbade
SpencerArtisan <spencerartisan@gmail.com>
2013-12-14 23:32:42 +0100

    Fix cache reloader

--- bin/reload_cache.sh
+++ bin/reload_cache.sh
@@ -2,13 +2,13 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'cinefile'
 
-def datasource
-  CachedDataSource.new(FilmAugmenter.new(Filter.new(FindAnyFilmDataSource.new, settings.max_cinemas), RottenTomatoes.new))
+def cache
+  Cache.new(FilmAugmenter.new(FilmGrouper.new(Filter.new(FindAnyFilmDataSource.new, settings.max_cinemas)), RottenTomatoes.new))
 end
 
 puts 'clearing cache'
-datasource.clear
+cache.clear
 
 puts 'loading cache'
-Film.all datasource, settings.lookahead, settings.postcode
+cache.get_films settings.postcode, settings.lookahead
 
