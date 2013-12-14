161e11f69f90d0d28a67ef5101e6987b3ffda3c4
SpencerArtisan <spencerartisan@gmail.com>
2013-12-15 00:44:50 +0100

    Show rating

--- lib/rotten_tomatoes.rb
+++ lib/rotten_tomatoes.rb
@@ -8,6 +8,7 @@ class RottenTomatoes
     url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{KEY}&q=#{CGI::escape(film.title)}&page_limit=1"
     puts "Calling #{url}"
     result = RestClient.get url
+    puts result
 
     json = JSON.parse result
     link = json["movies"][0]["links"]["alternate"] rescue nil
